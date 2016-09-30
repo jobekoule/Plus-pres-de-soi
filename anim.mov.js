c194532d58c4909a6d9512e79717c95e963ade12
admin <admin@MB-Pro.local>
2016-09-30 09:15:30 +0200

    + animation home

---
+++ anim.mov.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+			$('.rectangle')
+			.find('.square-small')
+			.animate({
+			left: "+=105%" },
+			speed = 3000
+
+				);
+
+			$('.rectangle')
+			.find('.square-small2')
+			.animate({
+			right: "+=45%" },
+			speed = 3000
+				);
+
+});
