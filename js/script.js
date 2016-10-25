f89c8eb28b49d3805f3d97eb06aa2f8bf310344d
girardpierre <girardpierre79@hotmail.fr>
2016-10-25 10:24:00 +0200

    ! home phrase accroche

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 $( function() {
-    /* dietetique - animation de conseil et recette*/
-    $('#recette a').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
+  /* dietetique - animation de conseil et recette*/
+  $('#recette a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
   });
-    $('#conseil a').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
+  $('#conseil a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
   });
-    $('.tabDietetique').click(function() {
-        $('.tabDietetique').removeClass('activeLien');
-        $(this).addClass('activeLien');
-    });
-    $('.required-icon').tooltip({
-        placement: 'left',
-        title: 'Required field'
-    });
+  $('.tabDietetique').click(function() {
+    $('.tabDietetique').removeClass('activeLien');
+    $(this).addClass('activeLien');
+  });
+  // changer la couleur de typo sur l'onglet actif de recette et conseil
+  $('.tabDietetique').click(function() {
+    $('.tabDietetique').removeClass('activeLien');
+    $(this).addClass('activeLien');
+  });
+  // changement de la classe active des onglets recette et conseil lors des choix de la page home
+  $('#boutonHomeConseil').click(function() {
+    $('#recette').removeClass('active');
+    $('#conseil').addClass('active');
+  });
+  $('.required-icon').tooltip({
+    placement: 'left',
+    title: 'Required field'
+  });
 });
